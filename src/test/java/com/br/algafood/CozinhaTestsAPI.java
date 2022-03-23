b59768f77d36e5c6b26617c801940d0b50affd7e
mauricio-sponton <sponton.lol@gmail.com>
2022-03-23 18:01:29 -0300

    Testes de API para Entidade Cozinha/ Novo Banco de Dados para testes

---
+++ src/test/java/com/br/algafood/CozinhaTestsAPI.java
@@ -0,0 +1,114 @@
+package com.br.algafood;
+
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import com.br.algafood.domain.model.Cozinha;
+import com.br.algafood.domain.repository.CozinhaRepository;
+import com.br.algafood.util.DatabaseCleaner;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
+class CozinhaTestsAPI {
+
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository repository;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+		
+		databaseCleaner.clearTables();
+		prepararDados();
+	}
+
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
+		
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(2));
+//			.body("nome", hasItems("Indiana", "Tailandesa"));
+	}
+	
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	@Test
+	public void deveRetornarRespostaEStatusCorretos_QuandoConsultarCozinhaExistente() {
+		given()
+		.pathParam("cozinhaId", 2)
+		.accept(ContentType.JSON)
+	.when()
+		.get("/{cozinhaId}")
+	.then()
+		.statusCode(HttpStatus.OK.value())
+		.body("nome", Matchers.equalTo("Americana"));
+	}
+	
+	@Test
+	public void deveRetornaStatus404_QuandoConsultarCozinhaInexistente() {
+		given()
+		.pathParam("cozinhaId", 100)
+		.accept(ContentType.JSON)
+	.when()
+		.get("/{cozinhaId}")
+	.then()
+		.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		
+		repository.save(cozinha1);
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		
+		repository.save(cozinha2);
+		
+	}
+
+}
