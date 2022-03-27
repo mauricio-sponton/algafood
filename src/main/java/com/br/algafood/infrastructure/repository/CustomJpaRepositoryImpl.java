61c6aa772d9d853e23057f4cdbc361c4c983fa95
mauricio-sponton <sponton.lol@gmail.com>
2022-03-27 00:04:38 -0300

    Regra para email duplicado

---
+++ src/main/java/com/br/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.br.algafood.infrastructure.repository;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import com.br.algafood.domain.repository.CustomJpaRepository;
+
+
+public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID>
+	implements CustomJpaRepository<T, ID> {
+
+	private EntityManager manager;
+	
+	public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, 
+			EntityManager entityManager) {
+		super(entityInformation, entityManager);
+		
+		this.manager = entityManager;
+	}
+
+	@Override
+	public void detach(T entity) {
+		manager.detach(entity);
+	}
+
+}
