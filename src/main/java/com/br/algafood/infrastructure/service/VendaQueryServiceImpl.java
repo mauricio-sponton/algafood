5779b483a0508d6e1e5721affed8d268b30bfa3f
mauricio-sponton <sponton.lol@gmail.com>
2022-04-01 02:28:23 -0300

    filtro para estatísticas de Vendas diárias

--- src/main/java/com/br/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/br/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,18 @@
 package com.br.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.br.algafood.domain.filter.VendaDiariaFilter;
 import com.br.algafood.domain.model.Pedido;
+import com.br.algafood.domain.model.StatusPedido;
 import com.br.algafood.domain.model.dto.VendaDiaria;
 import com.br.algafood.domain.service.VendaQueryService;
 
@@ -25,6 +28,8 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
 		
+		var predicates = new ArrayList<Predicate>();
+		
 		var functionDateDataCriacao = 
 				builder.function("date", Date.class, root.get("dataCriacao"));
 		
@@ -34,7 +39,25 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 				builder.sum(root.get("valorTotal"))
 		);
 		
+		if (filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+	      
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataCriacaoInicio()));
+		}
+
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status").in(
+				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(functionDateDataCriacao);
 		
 		
