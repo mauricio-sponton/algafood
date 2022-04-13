c5f8bda79ab13eb7b124638b3c5195dd3f73f860
mauricio-sponton <sponton.lol@gmail.com>
2022-04-13 01:12:32 -0300

    Links HATEOAS condicionais para Restaurantes

--- src/main/java/com/br/algafood/api/AlgaLinks.java
+++ src/main/java/com/br/algafood/api/AlgaLinks.java
@@ -50,6 +50,26 @@ public class AlgaLinks {
 				.listar(restauranteId)).withRel(rel);
 	}
 	
+	public Link linkToRestauranteAbertura(Long restauranteId, String rel) {
+	    return linkTo(methodOn(RestauranteController.class)
+	            .abrir(restauranteId)).withRel(rel);
+	}
+
+	public Link linkToRestauranteFechamento(Long restauranteId, String rel) {
+	    return linkTo(methodOn(RestauranteController.class)
+	            .fechar(restauranteId)).withRel(rel);
+	}
+
+	public Link linkToRestauranteInativacao(Long restauranteId, String rel) {
+	    return linkTo(methodOn(RestauranteController.class)
+	            .inativar(restauranteId)).withRel(rel);
+	}
+
+	public Link linkToRestauranteAtivacao(Long restauranteId, String rel) {
+	    return linkTo(methodOn(RestauranteController.class)
+	            .ativar(restauranteId)).withRel(rel);
+	}
+	
 	public Link linkToConfirmacaoPedido(String codigoPedido, String rel) {
 		return linkTo(methodOn(FluxoPedidoController.class).confirmar(codigoPedido)).withRel(rel);
 	}
--- src/main/java/com/br/algafood/api/assembler/RestauranteDTOAssembler.java
+++ src/main/java/com/br/algafood/api/assembler/RestauranteDTOAssembler.java
@@ -12,46 +12,60 @@ import com.br.algafood.api.model.RestauranteDTO;
 import com.br.algafood.domain.model.Restaurante;
 
 @Component
-public class RestauranteDTOAssembler extends RepresentationModelAssemblerSupport<Restaurante, RestauranteDTO>{
+public class RestauranteDTOAssembler extends RepresentationModelAssemblerSupport<Restaurante, RestauranteDTO> {
 
 	@Autowired
 	private ModelMapper modelMapper;
-	
+
 	@Autowired
 	private AlgaLinks algaLinks;
-	
+
 	public RestauranteDTOAssembler() {
 		super(RestauranteController.class, RestauranteDTO.class);
 	}
-	
+
 	@Override
-    public RestauranteDTO toModel(Restaurante restaurante) {
+	public RestauranteDTO toModel(Restaurante restaurante) {
 		RestauranteDTO restauranteModel = createModelWithId(restaurante.getId(), restaurante);
-        modelMapper.map(restaurante, restauranteModel);
-        
-        restauranteModel.add(algaLinks.linkToRestaurantes("restaurantes"));
-        
-        restauranteModel.getCozinha().add(
-                algaLinks.linkToCozinha(restaurante.getCozinha().getId()));
-        
-        restauranteModel.getEndereco().getCidade().add(
-                algaLinks.linkToCidade(restaurante.getEndereco().getCidade().getId()));
-        
-        restauranteModel.add(algaLinks.linkToRestauranteFormasPagamento(restaurante.getId(), 
-                "formas-pagamento"));
-        
-        restauranteModel.add(algaLinks.linkToRestauranteResponsaveis(restaurante.getId(), 
-                "responsaveis"));
-        
-        return restauranteModel;
-    }
-    
-    @Override
-    public CollectionModel<RestauranteDTO> toCollectionModel(Iterable<? extends Restaurante> entities) {
-        return super.toCollectionModel(entities)
-                .add(algaLinks.linkToRestaurantes());
-    }   
-	
+		modelMapper.map(restaurante, restauranteModel);
+
+		restauranteModel.add(algaLinks.linkToRestaurantes("restaurantes"));
+
+		if (restaurante.ativacaoPermitida()) {
+			restauranteModel.add(algaLinks.linkToRestauranteAtivacao(restaurante.getId(), "ativar"));
+		}
+
+		if (restaurante.inativacaoPermitida()) {
+			restauranteModel.add(algaLinks.linkToRestauranteInativacao(restaurante.getId(), "inativar"));
+		}
+
+		if (restaurante.aberturaPermitida()) {
+			restauranteModel.add(algaLinks.linkToRestauranteAbertura(restaurante.getId(), "abrir"));
+		}
+
+		if (restaurante.fechamentoPermitido()) {
+			restauranteModel.add(algaLinks.linkToRestauranteFechamento(restaurante.getId(), "fechar"));
+		}
+
+		restauranteModel.getCozinha().add(algaLinks.linkToCozinha(restaurante.getCozinha().getId()));
+
+		if (restaurante.getEndereco() != null) {
+			restauranteModel.getEndereco().getCidade()
+					.add(algaLinks.linkToCidade(restaurante.getEndereco().getCidade().getId()));
+		}
+
+		restauranteModel.add(algaLinks.linkToRestauranteFormasPagamento(restaurante.getId(), "formas-pagamento"));
+
+		restauranteModel.add(algaLinks.linkToRestauranteResponsaveis(restaurante.getId(), "responsaveis"));
+
+		return restauranteModel;
+	}
+
+	@Override
+	public CollectionModel<RestauranteDTO> toCollectionModel(Iterable<? extends Restaurante> entities) {
+		return super.toCollectionModel(entities).add(algaLinks.linkToRestaurantes());
+	}
+
 //	public List<RestauranteDTO> toCollectionModel(List<Restaurante> restaurantes) {
 //		return restaurantes.stream()
 //				.map(restaurante -> toModel(restaurante))
